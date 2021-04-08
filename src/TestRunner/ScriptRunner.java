e5b9486ebe7732150204cc2c9dcec7c9ee871a44
saisandeep2218 <65825124+saisandeep2218@users.noreply.github.com>
2021-04-09 01:29:18 +0530

    Commit-01-Jenkins

---
+++ src/TestRunner/ScriptRunner.java
@@ -0,0 +1,30 @@
+package TestRunner;
+
+import DriverScript.driverUtility;
+import GenericModules.genericModule;
+import TestScripts.HomePageTestScript;
+import TestScripts.LoginTestScript;
+import TestScripts.UserMgmtTPageTestScript;
+
+public class ScriptRunner extends driverUtility {
+
+	public static void main(String[] args) {
+		HomePageTestScript      hs = new HomePageTestScript();
+		UserMgmtTPageTestScript us = new UserMgmtTPageTestScript();
+		genericModule.readInputFromUser();
+		driverUtility.launchDriver_Url();
+//		genericModule.seDragandDrop();
+//		genericModule.seWindowHandle();
+		genericModule.seWindowAlerts();
+//		LoginTestScript.ValidateLoginPage();
+//		genericModule.seRightClick();
+//		hs.ValidateHomePage();
+//		us.Validatelink();
+//		us.ValidateCheckBox();
+//		us.ValidateDropDown();
+//		us.ValidateGetText();
+//		Sedriver.close();
+
+	}
+
+}
